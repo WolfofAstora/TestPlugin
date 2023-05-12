bd8cae0dcf8f5dbf1578c388e39d468f6ceb902b
Benjamin <beni.masoldt@gmx.de>
2023-05-12 22:19:19 +0200

    removed schedular

--- src/main/java/PluginFirst.java
+++ src/main/java/PluginFirst.java
@@ -4,18 +4,14 @@ import Commands.setCommands.setZahl;
 import Commands.testSet;
 import Player.PlayerInfo;
 import Player.argsChecker;
-import Schedular.checkIfActive;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Level;
 
 public class PluginFirst extends JavaPlugin {
 
-    private checkIfActive checkIfActive;
-
     public void onDisable() {
 
         super.onDisable();
-        checkIfActive.cancel();
         this.getLogger().log(Level.INFO, "First_Plugin couldnt be loaded!");
 
     }
@@ -25,9 +21,6 @@ public class PluginFirst extends JavaPlugin {
 
         super.onEnable();
         saveDefaultConfig();
-
-        checkIfActive = new checkIfActive(this);
-        checkIfActive.startSchedular();
 
         this.getLogger().log(Level.INFO, "First_Plugin successfully loaded!");
         this.getCommand("hello").setExecutor(new hello());
